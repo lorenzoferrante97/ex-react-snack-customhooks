5257bfd0f254e8448b2775c8d13d9045569d7179
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-23 12:07:06 +0200

    add: useDate

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 // start code
 
 import useToggle from './hooks/useToggle';
+import useDate from './hooks/useDate';
 
 function App() {
   const { isOn, toggle } = useToggle(false);
+  const currentDate = useDate();
 
   return (
     <>
       <main id="main">
         <h1>Toggle: {`${isOn}`}</h1>
         <button onClick={toggle}>Cambia valore del toggle</button>
+        <h2>Current date: {`${currentDate.toLocaleString()}`}</h2>
       </main>
     </>
   );
